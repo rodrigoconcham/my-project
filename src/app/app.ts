39356f0e7839bf4d9434e6c323bcb8e5070d5625
Rodrigo Concha <externo.ryc63@consorcio.cl>
2025-11-05 16:21:06 -0300

    ngmodel

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,24 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header} from './componets/header/header';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
+
+  userName : String = 'Angular';
+
+
   number: number = 0;
 
  addOne() {
 
     this.number++;
   }
+  
 }
