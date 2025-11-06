1e41b61ce7bd056b7169a1700960fdab78118f1b
Rodrigo Concha <externo.ryc63@consorcio.cl>
2025-11-06 12:43:28 -0300

    @for [object]

--- src/app/app.ts
+++ src/app/app.ts
@@ -11,5 +11,25 @@ import { FormsModule } from '@angular/forms';
 })
 export class App {
  movies: string[] = ['The Shawshank Redemption', 'The Godfather', 'The Dark Knight']; 
+ animals: any = [
+  {
+    id: 1,
+    name: "dog",
+    img: "https://cdn.pixabay.com/photo/2019/02/06/15/18/puppy-3979350_1280.jpg"
+  },
+  {
+    id: 2,
+    name: "cat",
+    img: "https://i.pinimg.com/originals/2c/34/33/2c34330ef8ceaaab2f113e37359bee65.jpg"
+  },
+  {
+    id: 3,
+    name: "bird",
+    img: "https://i.ytimg.com/vi/rKk4WZPi5iU/maxresdefault.jpg"
+  },
+];
+
+
+
 
 }
