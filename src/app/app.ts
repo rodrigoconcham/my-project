2b6d088cf90aea4a3e8d93e510fe2d06cdcf3aec
Rodrigo Concha <externo.ryc63@consorcio.cl>
2025-11-06 17:49:33 -0300

    @input

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,35 +1,16 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Header} from './componets/header/header';
+import { Header } from './componets/header/header';
 import { FormsModule } from '@angular/forms';
-
+import { child } from './componets/child/child';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header, FormsModule],
+  imports: [RouterOutlet, Header, FormsModule, child],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
- movies: string[] = ['The Shawshank Redemption', 'The Godfather', 'The Dark Knight']; 
- animals: any = [
-  {
-    id: 1,
-    name: "dog",
-    img: "https://cdn.pixabay.com/photo/2019/02/06/15/18/puppy-3979350_1280.jpg"
-  },
-  {
-    id: 2,
-    name: "cat",
-    img: "https://i.pinimg.com/originals/2c/34/33/2c34330ef8ceaaab2f113e37359bee65.jpg"
-  },
-  {
-    id: 3,
-    name: "bird",
-    img: "https://i.ytimg.com/vi/rKk4WZPi5iU/maxresdefault.jpg"
-  },
-];
+  Text: String = 'Variables desde el padre';
 
-
-
-
+  person: any = { name: 'John', age: 30 };
 }
---
+++ src/app/componets/child/child.ts
@@ -0,0 +1,16 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-child',
+  standalone: true,
+  imports: [],
+  templateUrl: './child.html',
+  styleUrl: './child.css',
+})
+export class child {
+  @Input()
+  msg: String = ''
+
+  @Input()
+  person: any;
+}
